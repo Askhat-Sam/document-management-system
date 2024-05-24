98d307cf3cc91be6104ac6a3595294a8478213ff
Askhat <Askhat.Sam@gmail.com>
2024-05-24 20:52:35 +0500

    feat: add "users" endpoint into UserController

--- src/main/java/com/finalproject/document/management/controller/UserController.java
+++ src/main/java/com/finalproject/document/management/controller/UserController.java
@@ -2,23 +2,23 @@ package com.finalproject.document.management.controller;
 
 import com.finalproject.document.management.entity.User;
 import com.finalproject.document.management.service.UserService;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping("/users")
+import java.util.List;
+
+@RestController
+@RequestMapping("/document-management")
 public class UserController {
     private UserService userService;
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
-    @RequestMapping("/getUser")
-    @ResponseBody
-    public User findById(Integer theId) {
-        System.out.println("HERE: " + userService.findById(theId));
-        return userService.findById(theId);
+    @RequestMapping("/users")
+    public List<User> showAdminUser(){
+        return userService.findAll();
     }
+
 
 }
--- src/main/java/com/finalproject/document/management/entity/Document.java
+++ src/main/java/com/finalproject/document/management/entity/Document.java
@@ -1,6 +1,7 @@
 package com.finalproject.document.management.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,6 +13,7 @@ import lombok.Setter;
 @Setter
 @Table(name="document")
 public class Document {
+    @Id
     private int id;
     private String documentReference;
     private String type;
