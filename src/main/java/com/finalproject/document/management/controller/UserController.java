61020c2a193d8fdc4babf61ed5d6a134c1435d02
Askhat <Askhat.Sam@gmail.com>
2024-05-24 23:10:28 +0500

    feat: add addNewUser method into UserController

--- src/main/java/com/finalproject/document/management/controller/UserController.java
+++ src/main/java/com/finalproject/document/management/controller/UserController.java
@@ -1,12 +1,10 @@
 package com.finalproject.document.management.controller;
 
+import com.finalproject.document.management.entity.Role;
 import com.finalproject.document.management.entity.User;
 import com.finalproject.document.management.service.UserService;
-import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,4 +26,43 @@ public class UserController {
         return userService.findById(id);
     }
 
+    @PostMapping("/addNewUser")
+    public String addNewUser(
+                             @RequestParam("userId") String userId,
+                             @RequestParam("firstName") String firstName,
+                             @RequestParam("lastName") String lastName,
+                             @RequestParam("email") String email,
+                             @RequestParam("password") String password,
+                             @RequestParam("active") int active,
+                             @RequestParam("roleId") String roleId,
+                             @RequestParam("userRole") String userRole){
+        // Generate bcrypt hash
+//        String passwordHashed = "{bcrypt}" + BCrypt.hashpw(password, BCrypt.gensalt());
+
+        // Create a new user
+        User newUser = new User(userId, firstName, lastName, email, password, 1);
+
+        // Add role to use
+        newUser.add(new Role(roleId,userRole));
+
+        userService.update(newUser);
+
+        return "redirect:/document-management/users";
+    }
+
+//    @PostMapping("/addNewUser")
+//    public String addNewUser(@RequestBody User user){
+////        // Generate bcrypt hash
+////        String passwordHashed = "{bcrypt}" + BCrypt.hashpw(password, BCrypt.gensalt());
+////
+////        // Create a new user
+////        User newUser = new User(userId, firstName, lastName, email, passwordHashed, 1);
+////
+////        // Add role to use
+////        newUser.add(new Role(roleId,userRole));
+//
+//        userService.update(user);
+//
+//        return "redirect:/document-management/users";
+//    }
 }
