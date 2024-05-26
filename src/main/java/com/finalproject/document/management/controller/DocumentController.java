c7550844afae8a65cc78360723e83d9c34f5f964
Askhat <Askhat.Sam@gmail.com>
2024-05-26 11:42:07 +0500

    feat: add getDocuments method into DocumentController

--- src/main/java/com/finalproject/document/management/controller/DocumentController.java
+++ src/main/java/com/finalproject/document/management/controller/DocumentController.java
@@ -1,18 +1,25 @@
 package com.finalproject.document.management.controller;
 
+import com.finalproject.document.management.entity.Document;
+import com.finalproject.document.management.service.DocumentService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/documents")
-public class DocumentController {
+import java.util.List;
 
+@RestController
+@RequestMapping("/document-management/documents")
+@AllArgsConstructor
+public class DocumentController {
+    private DocumentService documentService;
 
 
     @GetMapping("/getDocuments")
-    public String getDocuments(){
-        return
+    public List<Document> getDocuments(){
+        return documentService.findAll();
     }
 
 }
--- src/main/java/com/finalproject/document/management/service/DocumentServiceImpl.java
+++ src/main/java/com/finalproject/document/management/service/DocumentServiceImpl.java
@@ -3,12 +3,13 @@ package com.finalproject.document.management.service;
 import com.finalproject.document.management.entity.Document;
 import com.finalproject.document.management.repository.DocumentRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+@Service
 @AllArgsConstructor
 public class DocumentServiceImpl implements DocumentService{
     private DocumentRepository documentRepository;
-
     @Override
     public List<Document> findAll() {
         return documentRepository.findAll();
