c7e5bcbaa7c81c38ecfa926e6357972ede79d668
Askhat <Askhat.Sam@gmail.com>
2024-06-02 17:30:55 +0500

    feat: add DocumentStatusRepository

---
+++ src/main/java/com/finalproject/document/management/repository/DocumentStatusRepository.java
@@ -0,0 +1,7 @@
+package com.finalproject.document.management.repository;
+
+import com.finalproject.document.management.entity.DocumentStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DocumentStatusRepository extends JpaRepository<DocumentStatus, Integer> {
+}
