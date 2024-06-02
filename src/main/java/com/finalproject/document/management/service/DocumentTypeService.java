c1f2ddaa6467b8d403f546d5d31f571924593029
Askhat <Askhat.Sam@gmail.com>
2024-06-02 17:27:24 +0500

    feat: add DocumentTypeService interface

---
+++ src/main/java/com/finalproject/document/management/service/DocumentTypeService.java
@@ -0,0 +1,7 @@
+package com.finalproject.document.management.service;
+
+import com.finalproject.document.management.entity.DocumentType;
+
+public interface DocumentTypeService {
+    DocumentType findById(int id);
+}
